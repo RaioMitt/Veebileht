b7e5534c9667c494ba888860e235a583ed981104
Osvald Nigola <thorin.tammiskilp@gmail.com>
2023-11-29 13:10:26 +0200

    parandatud kommentaare (eemaldatud ebavajalikud) ja struktureeritud koodi

--- js/more.js
+++ js/more.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const scrollAmount = 300;  // kui palju korraga kerida
+    // paremale nupp
     document.getElementById('next-button').addEventListener('click', function() {
         const gallery = document.getElementById('gallery');
-        gallery.scrollLeft += 300;
+        gallery.scrollLeft += scrollAmount;
     });
+    // vasakule nupp
     document.getElementById('back-button').addEventListener('click', function() {
         const gallery = document.getElementById('gallery');
-        gallery.scrollLeft -= 300;
+        gallery.scrollLeft -= scrollAmount;
     });
 });
